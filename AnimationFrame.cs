ff8faff1983c35f25a85203ee9d218a63f41d716
adriehl <adriehl94@gmail.com>
2015-09-10 15:00:00 -0400

    Create AnimationFrame.cs
    Used in Animation, I haven't fiqured out how to import the file yet, I ask you guys when I see you next.

---
+++ AnimationFrame.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace KeysToInsanity.Code
+{
+    public class AnimationFrame
+    {
+        public Rectangle SourceRectangle { get; set; }
+        public TimeSpan Duration { get; set;}
+    }
+}
