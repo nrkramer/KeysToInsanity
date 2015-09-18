99d6ad4e41f6178123c150c45caa9766b4450709
adriehl <adriehl94@gmail.com>
2015-09-18 09:08:38 -0400

    New classes for characters

---
+++ KeysToInsanity/KeysToInsanity/Code/Entitys/TheGentleman.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KeysToInsanity
+{
+    class TheGentleman
+    {
+        private BasicSprite gentleman;
+        
+        private TheGentleman()
+            {
+
+         gentleman = new BasicSprite(game , "TempGentlman.png");
+            }
+    }
+}
