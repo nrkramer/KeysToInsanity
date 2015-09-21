45cb389ad2e4a6c0d0c5855dc2c9b9a61dbcf384
Luke Skinker <zynon911@hotmail.com>
2015-09-21 14:40:39 -0400

    Physics

---
+++ KeysToInsanity/KeysToInsanity/Code/Physics.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KeysToInsanity.Code
+{
+    class Physics
+    {
+        public bool isHeJumping;
+        public int gravity;
+
+    }
+}
