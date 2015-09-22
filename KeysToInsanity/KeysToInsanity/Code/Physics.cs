2368afe6b5333775794b990bfd2f97616a63403a
Luke Skinker <zynon911@hotmail.com>
2015-09-22 12:10:46 -0400

    Physics 2.0

--- KeysToInsanity/KeysToInsanity/Code/Physics.cs
+++ KeysToInsanity/KeysToInsanity/Code/Physics.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +10,48 @@ namespace KeysToInsanity.Code
     class Physics
     {
         public bool isHeJumping;
-        public int gravity;
+        public Vector2 position;
+        public Vector2 velocity;
+
+        public void Update(GameTime gameTime)
+        {
+            position += velocity;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                velocity.X = 3f;
+            }else if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                velocity.X = -3f;
+            }
+            else{
+                velocity.X = 0f;
+            }
+
+            if(Keyboard.GetState().IsKeyDown(Keys.Space) && isHeJumping == false)
+            {
+                position.Y -= 10f;
+                velocity.Y = -5f;
+                isHeJumping = true;
+            }
+            if(isHeJumping == true)
+            {
+                int n = 1;
+                velocity.Y = 0.15f + n;
+            }
+
+            if(position.Y >= 450)
+            {
+                isHeJumping = false;
+            }
+
+            if(isHeJumping == false)
+            {
+                velocity.Y = 0f;
+            }
+
+        }
+        
 
     }
 }
