533784c3d6b424c9db64b0a468a019537ea8dbc9
adriehl <adriehl94@gmail.com>
2015-10-05 15:05:59 -0400

    merge

--- KeysToInsanity/KeysToInsanity/Code/KeysToInsanity.cs
+++ KeysToInsanity/KeysToInsanity/Code/KeysToInsanity.cs
@@ -14,8 +14,8 @@ namespace KeysToInsanity
     public class KeysToInsanity : Game
     {
         // Some debug values
-        public static bool DRAW_BOUNDING_BOXES = true; // Draw bounding boxes on all sprites
-        public static bool DRAW_MOVEMENT_VECTORS = true;
+        public static bool DRAW_BOUNDING_BOXES = false; // Draw bounding boxes on all sprites
+        public static bool DRAW_MOVEMENT_VECTORS = false;
         public static Texture2D BOUNDING_BOX;
         public static Texture2D MOVEMENT_VECTOR;
 
