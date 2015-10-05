91a57b83d7b658046fe0734dffe1738093d1aaea
Luke Skinker <zynon911@hotmail.com>
2015-10-05 14:13:26 -0400

    Merge

--- KeysToInsanity/KeysToInsanity/Code/Physics.cs
+++ KeysToInsanity/KeysToInsanity/Code/Physics.cs
@@ -7,7 +7,9 @@ namespace KeysToInsanity.Code
     class Physics
     {
         //public Velocity gravity = Velocity.FromDirection(-90.0f, -9.8f);
-        public float gravity = 3.0f;
+        public float gravity = 9.8f;
+        public bool onTheGround;
+
         public void Update(GameTime gameTime, SpriteContainer spritesToPhysics)
         {
             //Console.WriteLine();
@@ -15,10 +17,13 @@ namespace KeysToInsanity.Code
             //gravity is applied to every sprite in the game here so that there is a universal gravity
             foreach (BasicSprite i in spritesToPhysics)
             {
-                    i.velocity.setY(i.velocity.getY()  + (gravity * frameTime));
-                    //Console.WriteLine(gravity * Velocity.FromCoordinates(frameTime, frameTime));
-                }
+                i.velocity.setY((i.velocity.getY() * 0.5f) + (gravity * frameTime));
+                if (i.velocity.getY() >= 29.0f)
+                   i.velocity.setY(29.0f);
                 
+
+               // Console.WriteLine(i.velocity.getY());
             }
         }
     }
+}
