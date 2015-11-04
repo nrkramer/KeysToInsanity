57d8592de1affe573b17465548b22681ae038ab6
Luke Skinker <zynon911@hotmail.com>
2015-11-04 15:07:14 -0500

    Death Screen

---
+++ KeysToInsanity/KeysToInsanity/Code/Interface/DeathScreen.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KeysToInsanity.Code
+{
+    class DeathScreen
+    {
+        //options for what will be on the death screen
+        private BasicSprite restartCP;
+        private BasicSprite restartL;
+        private BasicSprite exitToMenu;
+
+        //Used for position of the death screen        
+        private Vector2 restartCPButtonPosition;
+        private Vector2 restartLButtonPosition;
+        private Vector2 exitToMenuPosition;
+
+    }
+}
