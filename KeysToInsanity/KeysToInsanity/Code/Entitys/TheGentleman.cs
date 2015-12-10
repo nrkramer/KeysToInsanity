699d81bd96d41094dc6208df258fed6316569af0
adriehl <adriehl94@gmail.com>
2015-12-09 19:07:21 -0500

    Small Changes

--- KeysToInsanity/KeysToInsanity/Code/Entitys/TheGentleman.cs
+++ KeysToInsanity/KeysToInsanity/Code/Entitys/TheGentleman.cs
@@ -166,11 +166,11 @@ namespace KeysToInsanity.Code
 
             // run right
             Animation runRight = new Animation();
-            runRight.AddUniformStrip(new Rectangle(0, 50, 70, 32), new Point(16, 32), TimeSpan.FromSeconds(0.09));
+            runRight.AddUniformStrip(new Rectangle(0, 50, 70, 32), new Point(16, 32), TimeSpan.FromSeconds(0.10));
 
             // run left
             Animation runLeft = new Animation();
-            runLeft.AddUniformStrip(new Rectangle(0, 100, 70, 32), new Point(16, 32), TimeSpan.FromSeconds(0.09));
+            runLeft.AddUniformStrip(new Rectangle(0, 100, 70, 32), new Point(13, 32), TimeSpan.FromSeconds(0.10));
 
             // fall right
             Animation fallRight = new Animation();
